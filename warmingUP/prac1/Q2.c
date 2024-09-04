dfe33e5bdd5eafa9acbf961ff90fa1bc1fb7a67b
JHG@LT <rpg2657@gmail.com>
2024-09-04 15:17:05 +0900

    file open/close

--- warmingUP/prac1/Q2.c
+++ warmingUP/prac1/Q2.c
@@ -3,7 +3,33 @@
 #include <stdlib.h>
 #include <windows.h>
 
+typedef struct word{
+	struct word* next;
+	struct word* prev;
+	char w[15];
+}Word;
+
 int main() {
+	FILE* p_file;
+	char fileName[20] = { 0 };
+	char str[10000] = {0};
+
 	printf("input data file name : ");
+	scanf("%s", fileName);
+	fseek(stdin, 0, SEEK_END);
+	
+	p_file = fopen(fileName,"r");
+
+	if (NULL != p_file) {
+		fgets(str, sizeof(str), p_file);
+		printf("%s", str);
+		printf("%d", sizeof(Word));
+	}
+	else {
+		printf("fail");
+	}
+
+	fclose(p_file);
+
 	return 0;
 }
