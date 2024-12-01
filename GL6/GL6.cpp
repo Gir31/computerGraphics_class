8a59fb17e656238a4a48d58134021990a0602109
Gir31_PC <rpg2657@gmail.com>
2024-12-01 22:10:24 +0900

    조명 넣기

---
+++ GL6/GL6.cpp
@@ -0,0 +1,179 @@
+﻿#include "GL_My_header.h"
+//========================================================
+
+//========================================================
+// 원근 투영
+PROJECTION projection = { 45.0f, 0.0f, 0.1f, 50.0f };
+glm::vec3 spaceTrans = glm::vec3(0.0f, 0.0f, -2.0f);
+//========================================================
+char vertex[] = { "vertex.glsl" };
+char fragment[] = { "fragment.glsl" };
+GLuint shaderProgramID;
+//========================================================
+// 사용자 지정 변수
+float vertices[] = { //--- 버텍스 속성: 좌표값(FragPos), 노말값 (Normal)
+	-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
+	0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f, -0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f, -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
+	-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+	0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f, -0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f, -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+	-0.5f, 0.5f, 0.5f, -1.0f, 0.0f, 0.0f, -0.5f, 0.5f, -0.5f, -1.0f, 0.0f, 0.0f, -0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f,
+	-0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f, -0.5f, -0.5f, 0.5f, -1.0f, 0.0f, 0.0f, -0.5f, 0.5f, 0.5f, -1.0f, 0.0f, 0.0f,
+	0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+	0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f,
+	-0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f, 0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f, 0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f,
+	0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f, -0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f, -0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f,
+	-0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
+	0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f, -0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f, -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f
+};
+
+glm::vec3 cameraPos = glm::vec3(0.f, 0.f, 5.f);
+glm::vec3 cameraRotate = glm::vec3(0.f);
+
+
+GLUquadric* qobj;
+
+GLboolean timeSwitch = FALSE;
+//========================================================
+GLvoid drawScene();
+GLvoid Reshape(int w, int h);
+GLvoid Keyboard(unsigned char key, int x, int y);
+GLvoid SpecialKeyboard(int key, int x, int y);
+GLvoid TimerFunction(int value);
+void make_shaderProgram();
+void InitBuffer_();
+//========================================================
+// 사용자 지정 함수
+
+GLvoid cameraTranslation(glm::vec3 cameraTrans);
+
+//========================================================
+
+int main(int argc, char** argv)
+{
+	srand(time(NULL));
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(800.0f, 800.0f);
+	glutCreateWindow("Example1");
+
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	make_shaderProgram();
+	InitBuffer_();
+	glutDisplayFunc(drawScene);
+	glutReshapeFunc(Reshape);
+	glutKeyboardFunc(Keyboard);
+	glutSpecialFunc(SpecialKeyboard);
+	glutMainLoop();
+}
+
+GLvoid drawScene()
+{
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	Perspective_Projection_Transformation(projection, spaceTrans, shaderProgramID);
+	cameraTranslation(cameraPos);
+
+	glUseProgram(shaderProgramID);
+
+	glDrawArrays(GL_TRIANGLES, 0, 54);
+
+	glutSwapBuffers();
+}
+
+GLvoid Reshape(int w, int h)
+{
+	projection.Aspect = w / h;
+
+	glViewport(0, 0, w, h);
+}
+
+void make_shaderProgram()
+{
+	make_VertexShaders(vertex);
+	make_FragmentShaders(fragment);
+
+	shaderProgramID = glCreateProgram();
+	glAttachShader(shaderProgramID, vertexShader);
+	glAttachShader(shaderProgramID, fragmentShader);
+	glLinkProgram(shaderProgramID);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	glUseProgram(shaderProgramID);
+}
+
+void InitBuffer_() {
+	unsigned int VBO, VAO;
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0); //--- 위치 속성
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float))); //--- 노말 속성
+	glEnableVertexAttribArray(1);
+	
+	glUseProgram(shaderProgramID);
+	unsigned int lightPosLocation = glGetUniformLocation(shaderProgramID, "lightPos"); //--- lightPos 값 전달: (0.0, 0.0, 5.0);
+	glUniform3f(lightPosLocation, 0.0, 0.0, 5.0);
+	unsigned int lightColorLocation = glGetUniformLocation(shaderProgramID, "lightColor"); //--- lightColor 값 전달: (1.0, 1.0, 1.0) 백색
+	glUniform3f(lightColorLocation, 1.0, 1.0, 1.0);
+	unsigned int objColorLocation = glGetUniformLocation(shaderProgramID, "objectColor"); //--- object Color값 전달: (1.0, 0.5, 0.3)의 색
+	glUniform3f(objColorLocation, 1.0, 0.5, 0.3);
+	unsigned int viewPosLocation = glGetUniformLocation(shaderProgramID, "viewPos"); //--- viewPos 값 전달: 카메라 위치
+	glUniform3f(viewPosLocation, cameraPos.x, cameraPos.y, cameraPos.z);
+}
+
+GLvoid Keyboard(unsigned char key, int x, int y)
+{
+	switch (key) {
+	case 'q': case 'Q':
+		glutLeaveMainLoop();
+	}
+	glutPostRedisplay();
+}
+
+GLvoid SpecialKeyboard(int key, int x, int y) {
+	switch (key) {
+	case GLUT_KEY_LEFT:
+		cameraRotate.y -= 1.f;
+		break;
+	case GLUT_KEY_RIGHT:
+		cameraRotate.y += 1.f;
+		break;
+	case GLUT_KEY_UP:
+		break;
+	case GLUT_KEY_DOWN:
+		break;
+	}
+	glutPostRedisplay();
+}
+
+GLvoid TimerFunction(int value) {
+	glutPostRedisplay();
+
+
+	if (timeSwitch) glutTimerFunc(10, TimerFunction, 1);
+}
+
+GLvoid cameraTranslation(glm::vec3 cameraTrans) {
+	glm::vec3 zeroPoint = glm::vec3(0, 0, 0);
+	glm::mat4 view = glm::mat4(1.0f);
+	view = camera_locate(cameraTrans, zeroPoint) * rotate_camera(cameraRotate);
+
+	unsigned int viewLocation = glGetUniformLocation(shaderProgramID, "view");
+	glUniformMatrix4fv(viewLocation, 1, GL_FALSE, &view[0][0]);
+}
---
+++ GL6/vertex.glsl
@@ -0,0 +1,21 @@
+#version 330 core
+
+//--- in_Position: attribute index 0 
+//--- in_Color: attribute index 1
+
+layout (location = 0) in vec3 vPos; //--- 위치 변수: attribute position 0
+layout (location = 1) in vec3 vNormal; //--- 컬러 변수: attribute position 1
+
+out vec3 FragPos;
+out vec3 Normal;
+
+uniform mat4 model = mat4(1.0f); // 모델링변환
+uniform mat4 view = mat4(1.0f); // 뷰잉변환
+uniform mat4 projection = mat4(1.0f); // 투영변환
+
+void main(void) 
+{
+	gl_Position = projection * view * model * vec4(vPos, 1.0);
+	FragPos = vec3(model * vec4(vPos, 1.0));
+	Normal = vNormal;
+}
